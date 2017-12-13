1a44c5ebdeac5aed2e0cfa548fd819460ca4f600
armadyl010 <mastercape88@live.ca>
2017-12-12 21:27:56 -0500

    Fixed the script

--- ftblavagens.lua
+++ ftblavagens.lua
@@ -1,16 +1,14 @@
-local time = os.time()
-local w,h = term.getSize()
 local msg = "This computer will activate the lava gens\nduring daylight so we dont lose alot of energy"
 local warning = "*** It is recommended to have this computer running\n    to reduce power consumption by 20k RF ***"
+local w,h = term.getSize()
 
 while true do
 
-	local timer = textutils.formatTime(time, true)
+	time = os.time()
+	timer = textutils.formatTime(time, true)
 	local timeLenght = string.len(timer)
 	local newW = math.floor(w/2 - timeLenght/2)
-	local newH = h/2
-
-	term.setCursorPos(1,1)
+	local newH = (h/2)
 	term.setTextColor(colors.green)
 	print(msg)
 	local curX,curY = term.getCursorPos()
@@ -18,17 +16,18 @@ while true do
 	term.setCursorPos(1, curY)
 	term.setTextColor(colors.red)
 	print(warning)
-	term.setCursorPos(newW,newH)
 	term.setTextColor(colors.orange)
+	term.setCursorPos(newW,newH)
 	print(timer)
-
+	
+	if time < 6 or time > 18 then
+	  rs.setOutput("back", true)
+	else
+	  rs.setOutput("back", false)
+	end
+	
 	sleep(0.1)
 	term.clear()
-	
-	if time < 6 and time > 18 then -- if it's daylight then send a redstone output
-		rs.setOutput("back", true)
-	else
-		rs.setOutput("back", false)
-	end
+	term.setCursorPos(1,1)
 	
 end
